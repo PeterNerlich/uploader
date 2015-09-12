0765b43fd52d9b81db13c030c2d762134136d1ee
Peter Nerlich <peter.nerlich@googlemail.com>
2015-09-12 23:22:39 +0200

    first basic file reading TODO: how to extract folders?!?

--- res/js/upload.js
+++ res/js/upload.js
@@ -1,13 +1,17 @@
+var rawfiles = [];
+var processedfiles = [];
+
 window.onload = function(){
 	console.log('window ready');
 
 	function pd(e) {
+		e = e || window.event; // get window.event if e argument missing (in IE)
 		e.preventDefault();
 	}
-	document.addEventListener('dragenter', pd);
-	document.addEventListener('dragleave', pd);
-	document.addEventListener('dragover', pd);
-	document.addEventListener('drop', pd);
+	addEventHandler(document, 'dragenter', pd);
+	addEventHandler(document, 'dragleave', pd);
+	addEventHandler(document, 'dragover', pd);
+	addEventHandler(document, 'drop', pd);
 
 	d = document.getElementById('dragbox');
 	d.classList.remove('activated');
@@ -15,27 +19,32 @@ window.onload = function(){
 	dm.innerHTML = 'Drag your images here!';
 	if(window.FileReader) { 
 		addEventHandler(d, 'dragenter', function(e){
+			e = e || window.event; // get window.event if e argument missing (in IE)
 			pd(e);
 			console.log('dragenter()');
 			d.classList.add('activated');
 			dm.innerHTML = 'Drop your images here!';
 		});
 		addEventHandler(d, 'dragleave', function(e){
+			e = e || window.event; // get window.event if e argument missing (in IE)
 			pd(e);
 			console.log('dragleave()');
 			d.classList.remove('activated');
 			dm.innerHTML = 'Drag your images here!';
 		});
 		addEventHandler(d, 'dragover', function(e){
+			e = e || window.event; // get window.event if e argument missing (in IE)
 			pd(e);
 			console.log('dragover()');
 			d.classList.add('activated');
 		});
 		addEventHandler(d, 'drop', function(e){
+			e = e || window.event; // get window.event if e argument missing (in IE)
 			pd(e);
 			console.log('drop()');
 			d.classList.remove('activated');
 			dm.innerHTML = 'Drag your images here!';
+			addFiles(e);
 		});
 	} else {
 		d.classList.add('error');
@@ -43,15 +52,51 @@ window.onload = function(){
 	}
 };
 
-function addEventHandler(obj, evt, handler) {
-    if(obj.addEventListener) {
-        // W3C method
-        obj.addEventListener(evt, handler, false);
-    } else if(obj.attachEvent) {
-        // IE method.
-        obj.attachEvent('on'+evt, handler);
-    } else {
-        // Old school method.
-        obj['on'+evt] = handler;
-    }
+function addFiles(e) {
+	console.log('addFiles()');
+	var list = document.getElementById('filecue').getElementsByTagName('ul')[0];
+
+	var dt = e.dataTransfer;
+	var files = dt.files;
+	var loadedfiles = 0;
+	console.log(files.length+' files dropped');
+	for (var i=0; i<files.length; i++) {
+		var file = files[i];
+		var reader = new FileReader();
+
+		addEventHandler(reader, 'loadend', function(e, file) {
+			loadedfiles++;
+			console.log('file '+i+' loaded');
+			var bin = this.result; 
+			var li = document.createElement('li');
+			li.innerHTML = '<p>Loaded : '+file.name+' size '+file.size+' B</p>';
+			list.appendChild(li);
+		}.bindToEventHandler(file));
+
+		reader.readAsDataURL(file);
+	}
 }
+
+function addEventHandler(obj, evt, handler) {
+	if(obj.addEventListener) {
+		// W3C method
+		obj.addEventListener(evt, handler, false);
+	} else if(obj.attachEvent) {
+		// IE method.
+		obj.attachEvent('on'+evt, handler);
+	} else {
+		// Old school method.
+		obj['on'+evt] = handler;
+	}
+}
+
+Function.prototype.bindToEventHandler = function bindToEventHandler() {
+	var handler = this;
+	var boundParameters = Array.prototype.slice.call(arguments);
+	//create closure
+	return function(e) {
+		e = e || window.event; // get window.event if e argument missing (in IE)	 
+		boundParameters.unshift(e);
+		handler.apply(this, boundParameters);
+	}
+};
